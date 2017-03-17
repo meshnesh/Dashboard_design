4b9422b8612e2e2b63e954c73c0b3a3669c5ca06
meshnesh <tonnie.nesh@gmail.com>
2017-03-17 15:45:25 +0300

    added the plus minus functionality

--- js/app.js
+++ js/app.js
@@ -1,4 +1,18 @@
 $("#leftside-navigation .sub-menu > a").click(function(e) {
     $("#leftside-navigation ul ul").slideUp(), $(this).next().is(":visible") || $(this).next().slideDown(),
         e.stopPropagation()
-})
+});
+
+/*Plus animation*/
+var container = document.querySelector('.container');
+var bar = document.querySelector('.bar--horizontal');
+
+container.addEventListener('click', function() {
+    if (bar.classList.contains('plus')) {
+        bar.classList.remove('plus')
+        bar.classList.add('minus')
+    } else {
+        bar.classList.remove('minus')
+        bar.classList.add('plus')
+    }
+});
