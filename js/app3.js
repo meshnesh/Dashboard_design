8e36c231b9362a72dae20146ae61e78feda143fc
meshnesh <tonnie.nesh@gmail.com>
2017-03-22 12:02:55 +0300

    added toggle function on angle left

--- js/app3.js
+++ js/app3.js
@@ -1,9 +1,15 @@
+$(".nav-toggle-btn").click(function() {
+    $(".fa-angle-left").toggle();
+});
+
 ( function() {
     var mynav = $('body'),
         mybutton = mynav.find('.nav-toggle-btn');
+
 
     mybutton.on('click', function(e) {
         mynav.toggleClass('active-nav');
         e.preventDefault();
     });
 })();
+
